e6aa64c9c9cc38e7c059f2835968cb165ead47e9
cleo <docterwater@gmail.com>
2026-04-29 10:52:29 +0800

    為了部屬和開發相容

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // --- 加入這行啟動 CORS ---
+
+  // 讀取環境變數中的 CORS 來源，若無則預設為本地開發網址
+  const origins = process.env.CORS_ORIGINS
+    ? process.env.CORS_ORIGINS.split(',')
+    : ['http://localhost:5173'];
+
   app.enableCors({
-    origin: 'http://localhost:5173', // 准許你的前端存取
+    origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  await app.listen(process.env.PORT ?? 3000);
+  // --- 修改 Port 監聽 ---
+  // 必須監聽 0.0.0.0 且優先使用環境變數中的 PORT
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
